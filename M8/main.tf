11dde3942b5e2dd7042e317107c735902b7cfbf7
Dagnarok <dag.kilnes@gmail.com>
2024-05-15 12:34:04 +0200

    chore: Update storage account replication type to GRS

--- M8/main.tf
+++ M8/main.tf
@@ -6,15 +6,15 @@ resource "random_string" "random_string" {
 
 locals {
   workspaces_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
-  
-  rg_name = terraform.workspace == "default" ? "" : "${var.rg_name}-${local.workspaces_suffix}"
-  sa_name = terraform.workspace == "default" ? "" : "${var.sa_name}-${local.workspaces_suffix}"
+
+  rg_name    = terraform.workspace == "default" ? "" : "${var.rg_name}-${local.workspaces_suffix}"
+  sa_name    = terraform.workspace == "default" ? "" : "${var.sa_name}-${local.workspaces_suffix}"
   web_suffix = "<h1>${terraform.workspace}</h1>"
 }
 
 resource "azurerm_resource_group" "rg_web" {
-    name     = local.rg_name
-    location = var.location
+  name     = local.rg_name
+  location = var.location
 }
 
 resource "azurerm_storage_account" "sa_web" {
@@ -39,5 +39,5 @@ resource "azurerm_storage_blob" "index_html" {
 }
 
 output "primary_web_endpoint" {
-    value = azurerm_storage_account.sa_web.primary_web_endpoint
+  value = azurerm_storage_account.sa_web.primary_web_endpoint
 }
--- M8/variables.tf
+++ M8/variables.tf
@@ -1,30 +1,30 @@
-variable "rg_name" {    
-    type = string
-    description = "Resource Group Name"
-    default = "rg-web-dk"
+variable "rg_name" {
+  type        = string
+  description = "Resource Group Name"
+  default     = "rg-web-dk"
 }
 
 variable "sa_name" {
-    type = string
-    description = "Storage Account Name"
-    default = "sawebdk"
+  type        = string
+  description = "Storage Account Name"
+  default     = "sawebdk"
 }
 
 variable "location" {
-    type = string
-    description = "Location for the resources"
-    default = "westeurope"
+  type        = string
+  description = "Location for the resources"
+  default     = "westeurope"
 }
 
 variable "index_document" {
-    type = string
-    description = "Index document for the static website"
-    default = "index.html"
+  type        = string
+  description = "Index document for the static website"
+  default     = "index.html"
 }
 
 variable "source_content" {
-    type = string
-    description = "Source content for the static website"
-    default = "<h1>Hallo og velkommen til Modul 8</h1>"
+  type        = string
+  description = "Source content for the static website"
+  default     = "<h1>Hallo og velkommen til Modul 8</h1>"
 }
 
