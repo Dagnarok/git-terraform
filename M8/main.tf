c28349f5341a54da62ba457b2272367a3f902d21
Dagnarok <dag.kilnes@gmail.com>
2024-05-15 12:40:03 +0200

    chore: Refactor main.tf to improve readability and maintainability

--- M8/main.tf
+++ M8/main.tf
@@ -7,8 +7,8 @@ resource "random_string" "random_string" {
 locals {
   workspaces_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
 
-  rg_name    = terraform.workspace == "default" ? "" : "${var.rg_name}-${local.workspaces_suffix}"
-  sa_name    = terraform.workspace == "default" ? "" : "${var.sa_name}-${local.workspaces_suffix}"
+  rg_name    = terraform.workspace == "default" ? "${var.rg_name}" : "${var.rg_name}-${local.workspaces_suffix}"
+  sa_name    = terraform.workspace == "default" ? "${var.sa_name}" : "${var.sa_name}-${local.workspaces_suffix}"
   web_suffix = "<h1>${terraform.workspace}</h1>"
 }
 
