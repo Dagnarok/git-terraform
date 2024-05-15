9e82b047f3b53bf56d84dd5ca9c4c91d9abe604a
Dagnarok <dag.kilnes@gmail.com>
2024-05-15 10:41:05 +0200

    deploy

---
+++ M8/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.94.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-dk"
+    storage_account_name = "sabackenddkpxa7788paa"
+    container_name       = "scbackenddk"
+    key                  = "oving.m8.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
