c333bbcb12f979f7cbe68425cd68b77597bf8094
Dagnarok <dag.kilnes@gmail.com>
2024-05-23 15:05:39 +0200

    update

--- test/main.tf
+++ test/main.tf
@@ -7,6 +7,8 @@ resource "random_string" "random_string" {
 locals {
   workspaces_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
   rg-name           = "${var.rg-name}-${local.workspaces_suffix}"
+  sa-name           = "${var.sa-name}-${local.workspaces_suffix}"
+  web_suffix        = "<h1>${terraform.workspace}</h1>"
 }
 
 resource "azurerm_resource_group" "rg-name" {
@@ -20,5 +22,26 @@ resource "azurerm_storage_account" "sa-name" {
   location                 = azurerm_resource_group.rg-name.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  static_website {
+    index_document = var.index-document
+  }
 }
+
+resource "azurerm_storage_blob" "index-html" {
+  name                   = var.index-document
+  storage_account_name   = azurerm_storage_account.sa-name.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source_content         = "${var.source-content}${local.web_suffix}"
+}
+
+output "primary_web_endpoint" {
+  value = azurerm_storage_account.sa-name.primary_web_endpoint
+}
+
+
+
+
 
--- test/variables.tf
+++ test/variables.tf
@@ -12,3 +12,16 @@ variable "sa-name" {
   type        = string
   description = "value of the storage account name"
 }
+
+variable "index-document" {
+  type        = string
+  description = "Index document for the static website"
+  default     = "index.html"
+}
+
+variable "source-content" {
+  type        = string
+  description = "Source content for the static website"
+  default     = "<h1>Hallo og velkommen til Modul 8</h1>"
+}
+
