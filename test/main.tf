c4a25a9cafef207de6d3cad9b5d5db78e3103b24
Dagnarok <dag.kilnes@gmail.com>
2024-05-24 08:15:53 +0200

    account replication type from LRG to GRS

--- test/main.tf
+++ test/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_storage_account" "sa-name" {
   resource_group_name      = azurerm_resource_group.rg-name.name
   location                 = azurerm_resource_group.rg-name.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
 
   static_website {
     index_document = var.index-document
